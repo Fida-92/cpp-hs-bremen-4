82dc64a60d2a26185a6fcc14120588bdb39ded0d
Fida <fida@fida-VirtualBox>
2019-11-28 11:10:29 +0100

    Insert is working

--- list.h
+++ list.h
@@ -6,243 +6,177 @@
 #include <iostream>
 #include <stdexcept>
 namespace util {
-template <typename T>
-class Node
-{
+template <typename T> class Node {
 public:
-    // data type of element in node
-    T nodeData;
-    Node *prev;
-    Node *next;
-    bool last;
+  // data type of element in node
+  T nodeData;
+  Node *prev;
+  Node *next;
+  bool last;
 
-    // Default ctor
-    Node(): prev(nullptr), next(nullptr){
-
-    }
-    ~Node(){
-        delete prev;
-        delete next;
-    }
-    // getter and setter
-
+  // Default ctor
+  Node() : prev(nullptr), next(nullptr) {}
+  ~Node() {
+    delete prev;
+    delete next;
+  }
+  // getter and setter
+  void setLast(bool last) { this->last = last; }
+  bool gerLast() { return this->last; }
 };
 
-template<typename T>
-class list
-{
-
+template <typename T> class list {
 private:
-    Node<T> *head,*tail;
+  Node<T> *m_end = new Node<T>();
+  Node<T> *head = m_end, *tail = m_end;
+
 public:
+  // Iterator
+  class iterator {
+    friend class list;
+    Node<T> *m_pNode;
 
-    // Iterator
-    class iterator
-    {
-        friend class list;
-        Node<T>* m_pNode;
-    public:
-        Node<T>* getM_pNode(){ return this->m_pNode; }
-        iterator(Node<T>* pNode):m_pNode(pNode){}
-        inline bool operator==(const iterator& it) const{
-            return it.m_pNode == this->m_pNode;
-        }
-        inline bool operator!=(const iterator& it) const{
-            return !(it.m_pNode == this->m_pNode);
-        }
-        // pre-increment
-        iterator& operator++(){
-            m_pNode = m_pNode->next;
-
-            return *this;
-        }
-        // the int param. is to distinguish pre-increment from post-increment
-        // post-increment
-        iterator operator++(int){
-            iterator tmp = *this;
-            ++(*this);// using the pre-increment operator
-            return tmp;
-
-        }
-        T& operator*() const{
-            return m_pNode->nodeData;
-        }
-        T* operator->() const{
-            T tmpData = m_pNode->nodeData;
-            return &tmpData;
-        }
-    };
-    // end of iterator class
-
-    list<T>():head(nullptr),tail(nullptr){
-
+  public:
+    Node<T> *getM_pNode() { return this->m_pNode; }
+    iterator(Node<T> *pNode) : m_pNode(pNode) {}
+    inline bool operator==(const iterator &it) const {
+      return it.m_pNode == this->m_pNode;
     }
-    Node<T>& front()  {
-        return *this->head;
+    inline bool operator!=(const iterator &it) const {
+      return !(it.m_pNode == this->m_pNode);
     }
-    Node<T>& back() const{
-        return *this->tail;
-    }
-    bool empty() const {
-        return this->head == nullptr;
-    }
-    unsigned size() {
-        unsigned counter=0;
-        Node<T> *tmp = head;
-        while (head != nullptr) {
-            ++counter;
-            head = head->next;
-        }
-        head = tmp;
-        return counter;
-    }
-    void clear(){
-        this->head=nullptr;
-    }
+    // pre-increment
+    iterator &operator++() {
+      m_pNode = m_pNode->next;
 
-    void pop_front(){
-        if(head == nullptr){
-            throw std::invalid_argument("list is empty");
-            return;// nothing to "pop"
-        }else if(this->size() == 1){
-            this->head= nullptr;
-            this->tail= nullptr;
-        }else {
-            this->head = this->head->next;
-            this->head->prev = nullptr;
-        }
+      return *this;
     }
-
-    void pop_back(){
-        if(head == nullptr){
-            return;// nothing to "pop"
-        }else if(this->size() == 1){
-            this->head= nullptr;
-            this->tail= nullptr;
-        }else {
-            this->tail = this->tail->prev;
-            this->tail->next = nullptr;
-        }
+    // the int param. is to distinguish pre-increment from post-increment
+    // post-increment
+    iterator operator++(int) {
+      iterator tmp = *this;
+      ++(*this); // using the pre-increment operator
+      return tmp;
     }
-    void push_front(const T& t){
-        Node<T> *newNode = createNode(t);
-        if(head == nullptr){
-            handlePushesHeadIsNull(newNode);
-        }else {
-            newNode->next= this->head;
-            newNode->prev=nullptr;
-            this->head->prev = newNode;
-            this->head = newNode;
-        }
-
+    T &operator*() const { return m_pNode->nodeData; }
+    T *operator->() const {
+      T tmpData = m_pNode->nodeData;
+      return &tmpData;
     }
-    void push_back(const T& t){
-        Node<T> *newNode = createNode(t);
-        if(head == nullptr){
-            handlePushesHeadIsNull(newNode);
-        }else {
-            newNode->prev= this->tail;
-            this->tail->next = newNode;
-            this->tail = newNode;
-        }
+  };
+  // end of iterator class
+
+  list<T>() : head(nullptr), tail(nullptr) { m_end->setLast(true); }
+  Node<T> &front() { return *this->head; }
+  Node<T> &back() const { return *this->tail; }
+  bool empty() const { return this->head == nullptr; }
+  unsigned size() {
+    unsigned counter = 0;
+    Node<T> *tmp = head;
+    while (head != nullptr) {
+      ++counter;
+      head = head->next;
     }
+    head = tmp;
+    return counter;
+  }
+  void clear() { this->head = nullptr; }
 
-    void handlePushesHeadIsNull(Node<T> *newNode)
-    {
-        this->head = newNode;
-        this->head->prev=nullptr;
-        this->head->next=nullptr;
-        this->tail=newNode;
+  void pop_front() {
+    if (head == nullptr) {
+      throw std::invalid_argument("list is empty");
+      return; // nothing to "pop"
+    } else if (this->size() == 1) {
+      this->head = nullptr;
+      this->tail = nullptr;
+    } else {
+      this->head = this->head->next;
+      this->head->prev = nullptr;
     }
+  }
 
-    // iterator methods
-    iterator begin() const{
-        // soll einen iterator zurückliefern der
-        // auf das erste Element der Liste zeigt.
-        return iterator(head);
-
+  void pop_back() {
+    if (head == nullptr) {
+      return; // nothing to "pop"
+    } else if (this->size() == 1) {
+      this->head = nullptr;
+      this->tail = nullptr;
+    } else {
+      this->tail = this->tail->prev;
+      this->tail->next = nullptr;
     }
-    iterator end() const{
-        return iterator(tail);
+  }
+  void push_front(const T &t) {
+    Node<T> *newNode = createNode(t);
+    if (head == nullptr) {
+      handlePushesHeadIsNull(newNode);
+    } else {
+      newNode->next = this->head;
+      newNode->prev = nullptr;
+      this->head->prev = newNode;
+      this->head = newNode;
     }
-
-    iterator insert(iterator& it,const T& t){
-        Node<T> *elemNode = createNode(t);
-        std::cout << "insert h   " <<(it.m_pNode == head)<< std::endl;
-        if (head == nullptr) {
-            handlePushesHeadIsNull(elemNode);
-            std::cout << "insert head null "<<std::endl;
-            return iterator(elemNode);
-        }
-        if (this->size() == 1) {
-            std::cout << "insert size "<< this->size()<<std::endl;
-            elemNode->prev= nullptr;
-            elemNode->next= it.m_pNode;
-            it.m_pNode->prev = elemNode;
-            head = elemNode;
-            return iterator(head);
-        }
-        if (this->size() > 1) {
-            // std::cout << "insert size > 1? "<< this->size()<<std::endl;
-            if (it.m_pNode == head ) {
-                std::cout << "push front? "<< this->size()<<std::endl;
-                elemNode->prev= nullptr;
-                elemNode->next= it.m_pNode;
-                it.m_pNode->prev = elemNode;
-                head = elemNode;
-                return iterator(elemNode);
-            }else if (it.m_pNode == tail) {
-                std::cout << "tail? "<< this->size() <<std::endl;
-                elemNode->prev= tail->prev;
-                elemNode->next= it.m_pNode;
-                it.m_pNode->prev = elemNode;
-                it.m_pNode->next= nullptr;
-                return iterator(elemNode);
-            }else{
-                std::cout << "else? "<< this->size()<<std::endl;
-                elemNode->prev= it.m_pNode->prev;
-                elemNode->next= it.m_pNode;
-                if (it.m_pNode->prev != nullptr) {
-                    it.m_pNode->prev->next = elemNode;
-                }
-                it.m_pNode->prev = elemNode;
-                return iterator(elemNode);
-            }
-        }
-
-        std::cout << "insert    " <<it.m_pNode->nodeData << std::endl;
-        return iterator(elemNode);
+  }
+  void push_back(const T &t) {
+    Node<T> *newNode = createNode(t);
+    if (head == nullptr) {
+      handlePushesHeadIsNull(newNode);
+    } else {
+      newNode->prev = this->tail;
+      this->tail->next = newNode;
+      this->tail = newNode;
     }
-    Node<T> * createNode(const T& t){
-        Node<T> *elemNode = new Node<T>();
-        elemNode->nodeData = t;
-        return elemNode;
+  }
+
+  void handlePushesHeadIsNull(Node<T> *newNode) {
+    this->head = newNode;
+    this->head->prev = nullptr;
+    this->head->next = nullptr;
+    this->tail = newNode;
+  }
+
+  // iterator methods
+  iterator begin() const {
+    // soll einen iterator zurückliefern der
+    // auf das erste Element der Liste zeigt.
+    return iterator(head);
+  }
+  iterator end() const { return iterator(tail); }
+
+  iterator insert(iterator &it, const T &t) {
+    Node<T> *elemNode = createNode(t);
+    elemNode->prev = it.m_pNode->prev;
+    elemNode->next = it.m_pNode;
+    if (it.m_pNode->prev != nullptr) {
+      it.m_pNode->prev->next = elemNode;
+    } else {
+      head = elemNode;
     }
-    iterator erase(iterator& it){
-        if (this->size() < 1 || it.m_pNode == nullptr) {
-            return iterator(nullptr);
-        }
-        // 1.   2.   3.
-        // p=n  p=1  p=2
-        // n=2  n=3  n=n
-
-//        if (it.m_pNode->prev == nullptr) {
-//            it.m_pNode= it.m_pNode->next;
-
-//            return iterator(it.m_pNode);
-//        }
-//        if (it.m_pNode->next == nullptr) {
-//            it.m_pNode= it.m_pNode->prev;
-//            return iterator(it.m_pNode);
-//        }
-
-        it.m_pNode->prev->next = it.m_pNode->next;
-        it.m_pNode->next->prev = it.m_pNode->prev->next;
-        it.m_pNode=it.m_pNode->next;
-        return iterator(it.m_pNode);
+    it.m_pNode->prev = elemNode;
+    return iterator(elemNode);
+  }
+  Node<T> *createNode(const T &t) {
+    Node<T> *elemNode = new Node<T>();
+    elemNode->nodeData = t;
+    return elemNode;
+  }
+  iterator erase(iterator &it) {
+    if (it.m_pNode->prev != nullptr) {
+      it.m_pNode->prev->next = it.m_pNode->next;
+      it.m_pNode = it.m_pNode->next;
+    } else {
+      head = it.m_pNode->next;
+      it.m_pNode = head;
     }
-
+    if (it.m_pNode->next != nullptr) {
+      it.m_pNode->next->prev = it.m_pNode->prev;
+    } else {
+      tail = it.m_pNode->next;
+      it.m_pNode = tail;
+    }
+    return iterator(it.m_pNode);
+  }
 };
 
-}
+} // namespace util
 #endif // LIST_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // Author: Ahmadi, Fida
 // MatrikelNummer: 50101292
-#include <iostream>
 #include "list.h"
 #include <cassert>
+#include <iostream>
 void TestEmpty();
 void TestPush_front();
 void TestPush_back();
@@ -12,151 +12,142 @@ void TestPop_back();
 void TestPop_front();
 void TestFront();
 void TestBack();
-void TestBegin();
+void TestListEnd();
+void TestListBegin();
 void TestEraseWithIterator();
 void TestInsertWithIterator();
 using namespace util;
 int main() {
 
-    TestPush_front();
-    TestPush_back();
-    TestPop_back();
-    TestPop_front();
-    TestFront();
-    TestBack();
-    TestClear();
-    TestEmpty();
-    TestInsertWithIterator();
-    TestBegin();
-    return 0;
+  TestPush_front();
+  TestPush_back();
+  TestPop_back();
+  TestPop_front();
+  TestFront();
+  TestBack();
+  TestClear();
+  TestEmpty();
+  TestInsertWithIterator();
+  TestListBegin();
+  TestListEnd();
+  return 0;
 }
-//class Test{
-//public:
-//    int nummer;
-//    Test(int nummer):nummer(nummer){
-
-//    }
-//    void sysout(){
-//        std::cout<< this->nummer<< std::endl;
-//    }
-//};
-void TestBegin(){
-    list<int> li;
-    li.push_front(9);
-    li.push_front(19);
-    li.push_front(29);
-    // Node<int> *node = new Node<int>();
-    list<int>::iterator it(new Node<int>());
+void TestListEnd() {
+  list<int> li;
+  li.push_front(9);
+  li.push_front(19);
+  li.push_front(29);
+  list<int>::iterator it(new Node<int>());
+  assert(li.end().getM_pNode()->next == nullptr);
+  PrintTestResult(!li.empty(), "list.end()");
 }
-void TestInsertWithIterator(){
-    list<int> li;
-    li.push_back(19);
-    li.push_front(18);
-    Node<int> *node = new Node<int>();
-    node->nodeData=5;
-    list<int>::iterator it(&li.front());//&li.front()
-    it = li.insert(it,10);
-    it = li.insert(it,11);
-    it = li.insert(it,12);
-    it = li.insert(it,13);
-     it = li.erase(it);
-    std::cout << " vw "<<li.size()<< it.getM_pNode()->nodeData <<std::endl;
-    list<int>::iterator itB = li.begin();
-    list<int>::iterator itE = li.end();
-
-
-    std::cout << li.begin().getM_pNode()->nodeData <<std::endl;
-    std::cout << li.end().getM_pNode()->nodeData <<std::endl;
-
-    while(itB != itE) {
-        std::cout << "while-loop "<< *it <<std::endl;
-        it++;
-    }
-
-    assert(4==4);
-    // PrintTestResult(!li.empty(),"it.insert()");
-    //delete node;
-
+void TestListBegin() {
+  list<int> li;
+  li.push_front(9);
+  li.push_front(19);
+  li.push_front(29);
+  assert(li.begin().getM_pNode()->nodeData == 29);
+  PrintTestResult(!li.empty(), "list.begin()");
 }
-void TestEraseWithIterator(){
+void TestInsertWithIterator() {
+  list<int> li;
+  // li.push_back(19);
+  // li.push_front(18);
+  Node<int> *node = new Node<int>();
+  node->nodeData = 5;
+  list<int>::iterator it(node); // or &li.front() or &li.back()
+  li.insert(it, 10);
+  li.insert(it, 11);
+  li.insert(it, 12);
+  // std::cout << " List.size() " << li.size() << std::endl;
+  list<int>::iterator itB = li.begin();
+  list<int>::iterator itE = li.end();
 
+  while (itB != itE) {
+    std::cout << "iterator " << *itB << std::endl;
+    itB++;
+  }
+  assert(li.size() == 4);
+  PrintTestResult(!li.empty(), "it.insert()");
 }
-void TestPop_front(){
-    list<int> list;
-    list.push_front(9);
-    list.push_back(19);
-    list.push_back(29);
-    list.pop_front();
-    Node<int> i = list.front();
-    list.pop_front();
-    assert(list.size() == 1 && i.nodeData == 19);
-    PrintTestResult(!list.empty(),"pop_front()");
+void TestEraseWithIterator() {}
+void TestPop_front() {
+  list<int> list;
+  list.push_front(9);
+  list.push_back(19);
+  list.push_back(29);
+  list.pop_front();
+  Node<int> i = list.front();
+  list.pop_front();
+  assert(list.size() == 1 && i.nodeData == 19);
+  PrintTestResult(!list.empty(), "pop_front()");
 }
 
-void TestBack(){
-    list<int> list;
-    list.push_front(9);
-    list.push_back(19);
-    list.push_back(29);
-    list.pop_back();
-    assert(list.size() == 2 && list.back().nodeData == 19);
-    PrintTestResult(!list.empty(),"back()");
+void TestBack() {
+  list<int> list;
+  list.push_front(9);
+  list.push_back(19);
+  list.push_back(29);
+  list.pop_back();
+  assert(list.size() == 2 && list.back().nodeData == 19);
+  PrintTestResult(!list.empty(), "back()");
 }
 
-void TestFront(){
-    list<int> list;
-    list.push_front(9);
-    list.push_back(19);
-    list.push_back(29);
-    assert(list.size() == 3 && list.front().nodeData == 9);
-    PrintTestResult(!list.empty(),"front()");
+void TestFront() {
+  list<int> list;
+  list.push_front(9);
+  list.push_back(19);
+  list.push_back(29);
+  assert(list.size() == 3 && list.front().nodeData == 9);
+  PrintTestResult(!list.empty(), "front()");
 }
 
-void TestPop_back(){
-    list<int> list;
-    list.push_back(9);
-    list.push_back(19);
-    list.push_back(29);
-    list.pop_back();
-    assert(list.size() == 2);
-    PrintTestResult(!list.empty(),"pop_back()");
+void TestPop_back() {
+  list<int> list;
+  list.push_back(9);
+  list.push_back(19);
+  list.push_back(29);
+  list.pop_back();
+  assert(list.size() == 2);
+  PrintTestResult(!list.empty(), "pop_back()");
 }
 
-void TestPush_back(){
-    list<int> list;
-    list.push_back(9);
-    list.push_back(19);
-    assert(list.size() == 2 && list.back().nodeData == 19);
-    PrintTestResult(!list.empty(),"push_back()");
+void TestPush_back() {
+  list<int> list;
+  list.push_back(9);
+  list.push_back(19);
+  assert(list.size() == 2 && list.back().nodeData == 19);
+  PrintTestResult(!list.empty(), "push_back()");
 }
 
-void TestClear(){
-    list<int> list;
-    list.push_front(9);
-    list.push_front(19);
-    list.clear();
-    assert(list.size() == 0);
-    PrintTestResult(list.empty(),"clear()");
+void TestClear() {
+  list<int> list;
+  list.push_front(9);
+  list.push_front(19);
+  list.clear();
+  assert(list.size() == 0);
+  PrintTestResult(list.empty(), "clear()");
 }
 
-void TestPush_front(){
-    list<int> list;
-    list.push_front(9);
-    list.push_front(19);
-    list.push_front(29);
+void TestPush_front() {
+  list<int> list;
+  list.push_front(9);
+  list.push_front(19);
+  list.push_front(29);
 
-    assert(list.size() == 3);
-    PrintTestResult(!list.empty(),"push_front()");
-    list.clear();
-    assert(list.size() == 0);
+  assert(list.size() == 3);
+  PrintTestResult(!list.empty(), "push_front()");
+  list.clear();
+  assert(list.size() == 0);
 }
 
-void TestEmpty(){
-    list<int> list;
-    assert(list.empty());
-    PrintTestResult(list.empty(),"empty()");
+void TestEmpty() {
+  list<int> list;
+  assert(list.empty());
+  PrintTestResult(list.empty(), "empty()");
 }
-void PrintTestResult(bool isTrue, std::string str){
-    std::string info(isTrue ? "Successfully tested:     ": "Failed test of:     ");
-    std::cout<<info<<"\""<< str <<"\"."<<std::endl;
+void PrintTestResult(bool isTrue, std::string str) {
+  std::string info(isTrue ? "Successfully tested:     "
+                          : "Failed test of:     ");
+  std::cout << info << "\"" << str << "\"." << std::endl;
 }
