c0df23a28b8bd319b7a8678ae0c1dba8f5a2472f
Fida-92 <fida.ahmadi@t-online.de>
2019-11-28 22:56:14 +0100

    Repo URL

--- README.sh
+++ README.sh
@@ -1,7 +1,7 @@
 #Das Programm verwendet nur Standard Bibliotheken
 #Kompilieren: 
 # GitRepo 
-#https://github.com/Fida-92/cpp_hs_bremen
+#https://github.com/Fida-92/cpp_hs_bremen-4
 g++ main.cpp  list.h -o main
 
 #Ausführen : 
